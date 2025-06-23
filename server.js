cee53214000430a622ca86a4b1b5dd50cf95b237
RubberLucy <ravenwolf101008@gmail.com>
2025-06-23 14:32:48 +0300

    added the registration page, still working on the database, cant figure out how to submit new info from the page to the database much less check it with the login page, ill continue work tomorrow, WORK LEFT: about page, community page, creators page, main page game viewing functionality

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ console.log('Server is starting...');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const db = require('./server/db');
 
 // Creating instances
 const app = express();
@@ -13,7 +14,16 @@ const PORT = 1010;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// ...existing code...
+app.post('/api/register', (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
+  db.run('INSERT INTO users (firstName, lastName, email, password) VALUES (?, ?, ?, ?)', [firstName, lastName, email, password], function(err) {
+    if (err) {
+      return res.json({ success: false, message: 'Registration failed' });
+    }
+    res.json({ success: true, message: 'User registered!' });
+  });
+  res.json({ success: true, message: 'User registered!' });
+});
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
